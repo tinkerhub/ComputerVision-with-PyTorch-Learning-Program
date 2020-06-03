03cebfa7d39e2b99d8ec60b425d0e67316e9c28e
Abhinav K <abhinavk5544@gmail.com>
2020-06-03 22:44:32 +0530

    Linear regression

--- Tasks/registration_task/main.py
+++ Tasks/registration_task/main.py
@@ -5,17 +5,40 @@ x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
 y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 """
 
-import numpy
+import numpy as np
 
 class LinearRegression(object):
-  """
-  An implementation of linear regression model
-  """
-  
-  def fit(_input, _output):
-    pass
-    
-  def predict(_input):
-    pass
-    
-    
+    def __init__(self, mx=None, my=None, b_0=None, b_1=None):
+        self.mx=mx
+        self.my=my
+        self.b_0=b_0
+        self.b_1=b_1
+    def fit(self,_input, _output):
+        n=np.size(_output)
+        self.mx=np.mean(_input)
+        self.my=np.mean(_output)
+        sum1,sum2=0,0
+        for i in range(n):
+            sum1+=_input[i][0]*_output[i]
+            sum2+=_input[i][0]*_input[i][0]
+        SS_xy =sum1-n*self.my*self.mx 
+        SS_xx =sum2-n*self.mx*self.mx
+        self.b_1 = SS_xy / SS_xx 
+        self.b_0 = self.my - self.b_1*self.mx
+        print(self.b_0)
+        print(self.b_1)
+        print(self.my)
+        print(self.mx)
+        print(SS_xx)
+        print(SS_xy)
+        print(sum1)
+        print(sum2)
+      
+      
+    def predict(self,_input):
+        n=np.size(_input)
+        _output=[]
+        for i in range(n):
+            a=self.b_0+(self.b_1*_input[i][0])
+            _output.append(a)
+        return _output
