2d83901551167721308382e0d1568f7f8d859f17
KavyaV00 <44108726+KavyaV00@users.noreply.github.com>
2020-06-08 22:17:58 +0530

    Update main.py

--- Tasks/registration_task/main.py
+++ Tasks/registration_task/main.py
@@ -1,32 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
+"""
+Implement the linear regression model using python and numpy in the following class.
+The method fit() should take inputs like,
+x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
+y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""
 
-class LinearRegression:
+import numpy
 
-    def fit(self, X_train, Y_train):
-        self.X_train = [j for i in X_train for j in i]
-        self.Y_train = Y_train
-        self.model = np.polyfit(self.X_train,self.Y_train, 1)
+class LinearRegression(object):
+  """
+  An implementation of linear regression model
+  """
 
-        ## Simple model showing the relationship between x values and y values
-        # plt.scatter(self.X_train,self.Y_train)
-        # plt.title('X vs. Y')
-        # plt.xlabel('X')
-        # plt.ylabel('Y')
-        # plt.show()
+  def fit(_input, _output):
+    pass
 
-    def predict(self, X_test):
-        predict = np.poly1d(self.model)
-        Y_test = int(predict(X_test))
-        print('For X =' , X_test , ', Y =', Y_test)
+  def predict(_input):
+    pass
 
-## Trying the LinearRegression class for the given data
-# x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
-# y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# X = [j for i in x for j in i]
-
-# model = LinearRegression()
-# model.fit(X,y)
-
-# model.predict(10)
-# model.preduct(11)
