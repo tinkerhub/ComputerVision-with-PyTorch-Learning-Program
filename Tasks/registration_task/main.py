7264c2eca3da2d1f5f4fb71105e2f72fdae4d1b7
KavyaV00 <44108726+KavyaV00@users.noreply.github.com>
2020-06-08 22:31:46 +0530

    Update main.py

--- Tasks/registration_task/main.py
+++ Tasks/registration_task/main.py
@@ -11,12 +11,11 @@ class LinearRegression(object):
   """
   An implementation of linear regression model
   """
-
+  
   def fit(_input, _output):
     pass
-
+    
   def predict(_input):
     pass
-  
-  
-
+    
+    
