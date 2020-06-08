0ac83fbfffd23bcefdbd9db75d8c9588584830e1
randheerrrk <randheerrrk@gmail.com>
2020-06-08 12:52:54 +0530

    added folder

--- Tasks/registration_task/main.py
+++ Tasks/registration_task/main.py
@@ -5,17 +5,35 @@ x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
 y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 """
 
-import numpy
+import numpy as np
 
 class LinearRegression(object):
   """
   An implementation of linear regression model
   """
-  
-  def fit(_input, _output):
-    pass
+  def __init__(self):
+    self.W = None
+    self.b = None
+    self.lr = 0.001
+
+  def fit(self, X, y):
+    X = np.array(X)
+    m, train_X_shape= X.shape
+    self.W = np.zeros(train_X_shape)
+    self.b = 0
+    for _ in range(10000) :
+      #calculate prediction
+      y_pred = np.dot(X, self.W) + self.b
+      #calculate gradients for weight updation
+      dw = 1 / m * np.dot(X.T, (y_pred-y))
+      db = 1/ m * np.sum(y_pred-y)
     
-  def predict(_input):
-    pass
+      #weight updation
+      self.W = self.W - self.lr*dw
+      self.b = self.W - self.lr*db
+
+  def predict(self, X):
+    y_pred = np.floor((np.dot(X, self.W) + self.b))
+    return [int(x) for x in y_pred]
     
     
--- Tasks/registration_task/test.py
+++ Tasks/registration_task/test.py
@@ -6,11 +6,8 @@ y = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 model = LinearRegression()
 
 model.fit(x, y)
-
 linear_test_set = [([[10]], 12), ([[11]], 13)]
 
 @pytest.mark.parametrize(("ip", "op"), linear_test_set)
 def test_code(ip, op):
     assert int(model.predict(ip)[0]) == op
-
-
