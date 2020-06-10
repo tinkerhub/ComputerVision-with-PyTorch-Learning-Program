ce6378269a295a4ec0777fcdabe64c76fd229a33
jishnu <jishnusurajila@gmail.com>
2020-06-10 14:31:49 +0530

    Update task_d2.py

--- Tasks/daily tasks/jishnu/task_d2.py
+++ Tasks/daily tasks/jishnu/task_d2.py
@@ -3,10 +3,12 @@ import torch
 
 np.random.seed(0)
 
-array_a = np.random.randn(5,3)
-array_b = np.random.randn(3,4)
+#creating two random numpy arrays
+arr_a = np.random.randn(5,3)
+arr_b = np.random.randn(3,4)
 
-tensor_a = torch.tensor(array_a)
-tensor_b = torch.tensor(array_b)
+#converting numpy array to torch tensors
+tensor_a = torch.tensor(arr_a)
+tensor_b = torch.tensor(arr_b)
 
 print(tensor_a.matmul(tensor_b))
