eccbb33d7bbe613173b84c7e558ac0d3c25fa4ef
abhijeet1999 <38234788+abhijeet1999@users.noreply.github.com>
2020-06-11 15:12:51 +0530

    Add files via upload

--- Tasks/daily tasks/Abhijeet/task2.py
+++ Tasks/daily tasks/Abhijeet/task2.py
@@ -1,23 +1,31 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch
 
 class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
-        self.fc1 = nn.Linear(400, 120)
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(576, 120)
         self.fc2 = nn.Linear(120, 84)
         self.Output = nn.Linear(84, 10)
-        
+        self.sigmoid = nn.Sigmoid()
+
 
     def forward(self, x):
-        x= self.fc1(x)
-        x = nn.Sigmoid(x)
-        x=self.fc2(x)
-        x = nn.Sigmoid(x)
-        x = self.Output(x)
-        return x
+    	x=self.conv1(x)
+    	x = self.sigmoid(x)
+    	x=self.conv2(x)
+    	x = self.sigmoid(x)
+    	x= self.fc1(x)
+    	x = self.sigmoid(x)
+    	x=self.fc2(x)
+    	x = self.sigmoid(x)
+    	x = self.Output(x)
+    	return x
 
 
 net = Network()
 print(net)
+
