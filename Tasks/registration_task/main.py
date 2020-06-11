0ce55d76429bbb781f10144c53f61184e7ac37ee
abhijeet1999 <abhijeetcherungottil@gmail.com>
2020-06-11 15:09:17 +0530

    task2

---
+++ Tasks/registration_task/main.py
@@ -0,0 +1,21 @@
+"""
+Implement the linear regression model using python and numpy in the following class.
+The method fit() should take inputs like,
+x = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
+y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""
+
+import numpy
+
+class LinearRegression(object):
+  """
+  An implementation of linear regression model
+  """
+  
+  def fit(_input, _output):
+    pass
+    
+  def predict(_input):
+    pass
+    
+    
---
+++ projects/README.md
@@ -0,0 +1,2 @@
+# Projects
+Student projects once finished will be pushed to this monorepo as well! 
