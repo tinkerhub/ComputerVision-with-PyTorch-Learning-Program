689b702e8309432daed172a497ff0d07e41a3349
abhijeet1999 <abhijeetcherungottil@gmail.com>
2020-06-11 15:32:37 +0530

    task2

--- Tasks/daily tasks/Abhijeet/task2.py
+++ Tasks/daily tasks/Abhijeet/task2.py
@@ -1,31 +1,26 @@
 import torch.nn as nn
-import torch.nn.functional as F
+
 import torch
 
 class Network(nn.Module):
-    def __init__(self):
+    def __init__(self,N_in,N_out):
         super(Network, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        self.fc1 = nn.Linear(576, 120)
+        self.input = nn.Linear(N_in, 500)
+        self.fc1 = nn.Linear(500, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.Output = nn.Linear(84, 10)
+        self.Output = nn.Linear(84, N_out)
         self.sigmoid = nn.Sigmoid()
 
 
     def forward(self, x):
-    	x=self.conv1(x)
-    	x = self.sigmoid(x)
-    	x=self.conv2(x)
-    	x = self.sigmoid(x)
+    	x=self.input(x)
     	x= self.fc1(x)
     	x = self.sigmoid(x)
     	x=self.fc2(x)
-    	x = self.sigmoid(x)
     	x = self.Output(x)
     	return x
 
 
-net = Network()
+net = Network(600,10)
 print(net)
 
